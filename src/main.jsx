85b22df5f47c2fba069b10d99dcfce6477a1355d
Erick Andrade <erick_andradhe@hotmail.com>
2023-03-06 20:37:22 -0600

    feat(components):add test hooks to practice

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { HooksApp } from './HooksApp'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <MultipleCustomHooks/>
+    <Layout/>
   //</React.StrictMode>,
 )
