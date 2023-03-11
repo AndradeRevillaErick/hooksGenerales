108ac38ba713988af15d63dd6296ddb55c061ba8
Erick Andrade <erick_andradhe@hotmail.com>
2023-03-10 23:18:45 -0600

    feat(components):add more hooks exercises

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+import { ShowIncrement } from "./ShowIncrement";
+
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState(10);
+
+    const incrementFather = useCallback( (value) => {
+        setCounter( (c) => c + value );
+    }, []);
+
+    //ciclo infinito
+    // useFetch(() => {
+    //     incrementFather();
+    // }, [counter]);
+
+    // const incrementFather = () => {
+    //     setCounter( counter + 1 );
+    // }
+
+    return (
+        <>
+            <h1>useCallBack Hook: { counter }</h1>
+            <hr />
+
+            <ShowIncrement increment={ incrementFather }/>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CallbackHook } from './06-memos/CallbackHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
+// import { MemoHook } from './06-memos/MemoHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -7,11 +11,13 @@ import ReactDOM from 'react-dom/client'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Layout/>
+  // <CallbackHook/>
+    <Padre/>
   //</React.StrictMode>,
 )
