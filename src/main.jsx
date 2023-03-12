5436ca4db3c32b8d5781671821c0cb6d456299ac
Erick Andrade <erick_andradhe@hotmail.com>
2023-03-11 22:40:51 -0600

    feat(components): add initialstate for the reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { CallbackHook } from './06-memos/CallbackHook'
+// import { Padre } from './07-tarea-memo/Padre'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -13,13 +13,15 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
+import { TodoApp } from './08-useReducer/TodoApp'
 import './index.css'
 
-import './08-useReducer/intro-reducer';
+// import './08-useReducer/intro-reducer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
   // <CallbackHook/>
-    <Padre/>
+    // <Padre/>
+    <TodoApp/>
   //</React.StrictMode>,
 )
